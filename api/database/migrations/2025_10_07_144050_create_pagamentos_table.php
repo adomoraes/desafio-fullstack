d2f490ec7a6862563742b89a649e1debdca9b54a
Eduardo Moraes <adomoraes@gmail.com>
2025-10-07 11:48:19 -0300

    feat(api): cria migration da tabela de pagamentos

---
+++ api/database/migrations/2025_10_07_144050_create_pagamentos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pagamentos', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('contrato_id')->constrained('contratos');
+            $table->decimal('amount', 10, 2);
+            $table->dateTime('payment_date')->nullable();
+            $table->dateTime('due_date');
+            $table->string('status')->default('pendente');
+            $table->decimal('credit_used', 10, 2)->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pagamentos');
+    }
+};
