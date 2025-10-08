c27fa8dd7d67ea87d15c4802710c8e6b549ca0bc
Eduardo Moraes <adomoraes@gmail.com>
2025-10-08 14:12:36 -0300

    feat: Adicionar elementos básicos da interface do usuário
    - Cabeçalho adicionado com título
    - CSS do Tailwind configurado

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -5,6 +5,11 @@ export function App() {
 
   return (
     <BrowserRouter>
+      <div className="bg-red-500">
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </div>
       <Router />
     </BrowserRouter>
   )
