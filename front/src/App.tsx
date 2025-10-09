c7a391993690b068a268de1c57d341d55d4e2f4b
Eduardo Moraes <adomoraes@gmail.com>
2025-10-09 03:37:12 -0300

    feat: Remover fundo vermelho e texto "Olá, mundo"
    - Remover div desnecessária
    - Limpar componente raiz

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,17 +1,10 @@
-import { BrowserRouter } from "react-router-dom";
-import { Router } from "./Router";
+import { BrowserRouter } from "react-router-dom"
+import { Router } from "./Router"
 
 export function App() {
-
-  return (
-    <BrowserRouter>
-      <div className="bg-red-500">
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-      </div>
-      <Router />
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Router />
+		</BrowserRouter>
+	)
 }
-
