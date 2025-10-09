06fbdbecc40934ba78e0c982726f375ac601eeb5
Eduardo Moraes <adomoraes@gmail.com>
2025-10-09 05:53:32 -0300

    feat: Adicionar componente  header
    - Adicionado header com informações do usuário
    - Obter dados do usuário da API
    - Exibir estados de loading/erro

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,14 @@
 import { BrowserRouter } from "react-router-dom"
 import { Router } from "./Router"
+import { Header } from "./components/Header"
 
 export function App() {
 	return (
 		<BrowserRouter>
-			<Router />
+			<Header />
+			<main className="pt-16">
+				<Router />
+			</main>
 		</BrowserRouter>
 	)
 }
